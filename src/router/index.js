22b725b943087163a69fd7522cc759c8bdcbed41
plumCherry <412032039@qq.com>
2017-08-13 23:15:21 +0800

    ele project

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import VueRouter from "vue-router"
+
+import goods from "../components/goods/goods.vue"
+import rating from "../components/ratings/ratings.vue"
+import seller from "../components/seller/seller.vue"
+
+Vue.use(VueRouter);
+
+export default new VueRouter({
+  routes: [
+    {path: '/goods',component: goods},
+    {path: '/rating',component: rating},
+    {path: '/seller',component: seller},
+    {path: '/',redirect: '/goods'}
+  ]
+})
